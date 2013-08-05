25e968bf3dc35a5ea681df53a44cf0fe738b4ee2
Piratonym <piratonym@piratenfraktion-nrw.de>
2013-08-05 12:35:31 +0200

    Clean up: Moved templates and classes to external files.

---
+++ models/issue.rb
@@ -0,0 +1,49 @@
+class Issue < ActiveResource::Base
+  headers['X-Redmine-API-Key'] = APIKEY
+  self.site = 'https://redmine.piratenfraktion-nrw.de/'
+  self.format = :xml
+
+  def end_date
+    get_field 'End Datum'
+  end
+
+  def end_time
+    get_field 'End Uhrzeit'
+  end
+
+  def end_datetime
+    dt = DateTime.parse("#{end_date}T#{end_time}:00+02:00")
+    dt
+  end
+
+  def get_field(name)
+    custom_fields.select{ |f| f.name == name }.first.value rescue false
+  end
+
+  def pro; get_names("Dafür"); end
+  def count_pro; get_count("Dafür"); end
+
+  def contra; get_names("Dagegen"); end
+  def count_contra; get_count("Dagegen"); end
+
+  def abstitution; get_names("Enthaltung"); end
+  def count_abstitution; get_count("Enthaltung"); end
+
+  def not_voted; get_names("Noch nicht abgestimmt"); end
+  def count_not_voted; get_count("Noch nicht abgestimmt"); end
+
+  def not_participating; get_names("Nimmt nicht Teil"); end
+  def count_not_participating; get_count("Nimmt nicht Teil"); end
+
+  def get_names(field)
+    custom_fields.select { |f| f.value == field }.map{|f| f.name }.join(',')
+  end
+
+  def get_count(field)
+    custom_fields.select { |f| f.name if f.value == field }.length
+  end
+
+  def get_binding
+    binding
+  end
+end
--- redmine_bot.rb
+++ redmine_bot.rb
@@ -7,6 +7,9 @@ require 'active_resource'
 require 'erb'
 require 'net/smtp'
 
+require 'models/user.rb'
+require 'models/issue.rb'
+
 if ARGV.length != 4
   puts "usage: redmine_bot <mode (renew|umlauf|unhold|inventarcheck|inventarmails)> <username> <password> <api_key>"
   exit(-1)
@@ -17,85 +20,13 @@ USERNAME = ARGV[1]
 PASSWORD = ARGV[2]
 APIKEY   = ARGV[3]
 
-template = %q{{{Umlaufbeschluss
-|Startdatum=<%= start_date %>
-|Enddatum=<%= end_date %>
-|Enduhrzeit=<%= end_time %>
-|Dafür=<%= pro %>
-|Anzahl Dafür=<%= count_pro %>
-|Dagegen=<%= contra %>
-|Anzahl Dagegen=<%= count_contra %>
-|Enthaltung=<%= abstitution %>
-|Anzahl Enthaltung=<%= count_abstitution %>
-|Ohne Teilnahme=<%= not_participating %>
-|Anzahl ohne Teilnahme=<%= count_not_participating %>
-|Nicht abgestimmt=<%= not_voted %>
-|Anzahl ohne Stimme=<%= count_not_voted %>
-|Beschlussthema=<%= subject %>
-|Beschlusstext=<%= description %>
-}}}
-
-class User < ActiveResource::Base
-
-end
-
-class Issue < ActiveResource::Base
-  headers['X-Redmine-API-Key'] = APIKEY
-  self.site = 'https://redmine.piratenfraktion-nrw.de/'
-  self.format = :xml
-
-  def end_date
-    get_field 'End Datum'
-  end
-
-  def end_time
-    get_field 'End Uhrzeit'
-  end
-
-  def end_datetime
-    dt = DateTime.parse("#{end_date}T#{end_time}:00+02:00")
-    dt
-  end
-
-  def get_field(name)
-    custom_fields.select{ |f| f.name == name }.first.value rescue false
-  end
-
-  def pro; get_names("Dafür"); end
-  def count_pro; get_count("Dafür"); end
-
-  def contra; get_names("Dagegen"); end
-  def count_contra; get_count("Dagegen"); end
-
-  def abstitution; get_names("Enthaltung"); end
-  def count_abstitution; get_count("Enthaltung"); end
-
-  def not_voted; get_names("Noch nicht abgestimmt"); end
-  def count_not_voted; get_count("Noch nicht abgestimmt"); end
-
-  def not_participating; get_names("Nimmt nicht Teil"); end
-  def count_not_participating; get_count("Nimmt nicht Teil"); end
-
-  def get_names(field)
-    custom_fields.select { |f| f.value == field }.map{|f| f.name }.join(',')
-  end
-
-  def get_count(field)
-    custom_fields.select { |f| f.name if f.value == field }.length
-  end
-
-  def get_binding
-    binding
-  end
-end
-
 if MODE == "umlauf"
   mw = MediaWiki::Gateway.new('https://wiki.piratenfraktion-nrw.de/api.php')
   mw.login(USERNAME, PASSWORD, 'Piratenfraktion NRW')
 
   umlaufbeschluesse = Issue.find(:all, :params => { :tracker_id => 13 })
   umlaufbeschluesse.each do |u|
-    result = ERB.new(template).result(u.get_binding)
+    result = ERB.new(File.read('tpl/umlaufbeschluss.erb')).result(u.get_binding)
     page_name = 'Protokoll:Beschlüsse/' + u.start_date + '_' + u.subject
     mw.edit(page_name, result, :summary => 'RedmineBot')
     if DateTime.now > u.end_datetime
@@ -122,29 +53,13 @@ elsif MODE == 'unhold'
   end
 elsif MODE == "inventarcheck"
   inventar = Issue.find(:all, :params => { :status_id => 18  })
-  Net::SMTP.start('mail.piratenfraktion-nrw.de', 25, 'piratenfraktion-nrw.de', "#{USERNAME}", "#{PASSWORD}") do |smtp|
+  Net::SMTP.start('mail.piratenfraktion-nrw.de', 587, 'piratenfraktion-nrw.de', "#{USERNAME}", "#{PASSWORD}") do |smtp|
     inventar.each do |i|
       dt = DateTime.parse("#{i.due_date}")
       if DateTime.now >= dt
         #puts "Inventaritem in Ticket \##{i.id} ist heute fällig"
         Issue.put(i.id, :issue => { :status_id => 22 })
-        msg = <<END_OF_MESSAGE
-From: Fraktions-IT-Autoreminder <it+redmine@piratenfraktion-nrw.de>
-To: #{i.assigned_to.name} <#{User.find(:first, :params => { :id => i.assigned_to.id }).mail }>
-Subject: [Inventar - Inventar \##{i.id}] (Fällig) #{i.subject}
-Date: #{DateTime.now}
-
-Hallo,
-bitte gib das von dir ausliehene Objekt "#{i.subject}" (#{i.get_field('Typ')} von #{i.get_field('Hersteller')}) heute bei der Fraktions-IT zurück. Du hast dieses am #{i.start_date} erhalten, dabei wurde der heutige Tag als Rückgabedatum festgelegt.
-
-Falls du das Objekt heute nicht zurückgeben kannst oder länger behalten willst, wende dich bitte an die Fraktions-IT.
-
-Nähere Informationen findest du hier:
-> https://redmine.piratenfraktion-nrw.de/issues/#{i.id}
-
--- 
-Diese Mail wurde automatisiiert vom Inventarbot der Fraktions-IT verschickt.
-END_OF_MESSAGE
+        msg = ERB.new(File.read('tpl/inventar_faellig.erb')).result(i.get_binding)
         smtp.send_message msg, 'it+redmine@piratenfraktion-nrw.de', User.find(:first, :params => { :id => i.assigned_to.id }).mail
       end
     end
@@ -159,25 +74,9 @@ END_OF_MESSAGE
   end
 elsif MODE == "inventarmails"
   inventar = Issue.find(:all, :params => { :status_id => 21 })
-  Net::SMTP.start('mail.piratenfraktion-nrw.de', 25, 'piratenfraktion-nrw.de', "#{USERNAME}", "#{PASSWORD}") do |smtp|
+  Net::SMTP.start('mail.piratenfraktion-nrw.de', 587, 'piratenfraktion-nrw.de', "#{USERNAME}", "#{PASSWORD}") do |smtp|
     inventar.each do |i|
-      msg = <<END_OF_MESSAGE
-From: Fraktions-IT-Autoreminder <it+redmine@piratenfraktion-nrw.de>
-To: #{i.assigned_to.name} <#{User.find(:first, :params => { :id => i.assigned_to.id }).mail}>
-Subject: [Inventar - Inventar \##{i.id}] (Überfällig) #{i.subject}
-Date: #{DateTime.now}
-
-Hallo,
-das von dir ausliehene Objekt "#{i.subject}" (#{i.get_field('Typ')} von #{i.get_field('Hersteller')}) hat den Ausleihezeitraum überschritten. Du hast dieses am #{i.start_date} erhalten, dabei wurde der #{i.due_date} als Rückgabedatum festgelegt.
-
-Bitte gib das Objekt so schnell wie möglich zurück oder wende dich an die Fraktions-IT. Bis dahin werden regelmäßig weitere Erinnerungsmails an dich gesendet werden.
-
-Nähere Informationen findest du hier:
-> https://redmine.piratenfraktion-nrw.de/issues/#{i.id}
-
--- 
-Diese Mail wurde automatisiiert vom Inventarbot der Fraktions-IT verschickt.
-END_OF_MESSAGE
+      msg = ERB.new(File.read('tpl/inventar_ueberfaellig.erb')).result(i.get_binding)
       smtp.send_message msg, 'it+redmine@piratenfraktion-nrw.de', User.find(:first, :params => { :id => i.assigned_to.id }).mail
     end
   end
